abfdf74b5af2ab40fb3b291c887f327c2da856c3
Hijikata1869 <liv.knk@gmail.com>
2024-02-11 21:23:09 +0900

    tailwindでスタイルを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,41 @@
 export const App: React.FC = () => {
   return (
     <>
-      <div>
-        <div>
-          <p>TODOを入力</p>
+      <div className="flex flex-col w-1/3">
+        <div className="flex flex-col justify-center items-center p-4 bg-blue-300">
+          <p className="pb-2 text-gray-700">TODOを入力</p>
           <form>
-            <input type="text" />
-            <button>追加</button>
+            <input type="text" className="rounded-md mb-6 w-80 outline-none" />
+            <button className="ml-2 px-2 py-1 rounded-full text-sm text-gray-50 bg-gray-400">
+              追加
+            </button>
           </form>
         </div>
-        <div>
-          <p>未完了のTODOリスト</p>
-          <ul>
-            <li>
-              <div>
+        <div className="flex flex-col p-4 bg-red-300 min-h-[300px]">
+          <p className="pb-4 text-gray-700 text-center">未完了のTODOリスト</p>
+          <ul className="flex flex-col pl-6">
+            <li className="list-disc">
+              <div className="flex">
                 <p>未完了のTODO</p>
-                <button>完了</button>
-              </div>
-            </li>
-            <li>
-              <div>
-                <p>未完了のTODO</p>
-                <button>完了</button>
+                <button className="ml-2 px-2 py-1 rounded-full text-sm text-gray-50 bg-gray-400">
+                  完了
+                </button>
               </div>
             </li>
           </ul>
         </div>
-        <div>
-          <p>完了したTODO</p>
-          <ul>
-            <li>
-              <div>
+        <div className="flex flex-col p-4 bg-green-300 min-h-[300px]">
+          <p className="pb-4 text-gray-700 text-center">完了したTODO</p>
+          <ul className="flex flex-col pl-6">
+            <li className="list-disc">
+              <div className="flex">
                 <p>完了したTODO</p>
-                <button>戻す</button>
-                <button>削除</button>
+                <button className="ml-2 px-2 py-1 rounded-full text-sm text-gray-50 bg-gray-400">
+                  戻す
+                </button>
+                <button className="ml-2 px-2 py-1 rounded-full text-sm text-gray-50 bg-gray-400">
+                  削除
+                </button>
               </div>
             </li>
           </ul>
